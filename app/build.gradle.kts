2bcb7cbfb70c8fe31749d99a475d05ea7ddd64cb
devid_slesarev <d.slesarev@student.csn.khai.edu>
2023-11-30 14:37:45 +0200

    Домашние окно и Диалог
    Полность сделал базовый макет домашней активности. Начал разработку полноэкранного диалога для демонстрации активного аккаунта.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -54,6 +54,10 @@ dependencies {
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     implementation("androidx.navigation:navigation-fragment-ktx:2.7.5")
     implementation("androidx.navigation:navigation-ui-ktx:2.7.5")
+
+    implementation("com.readystatesoftware.systembartint:systembartint:1.0.3")
+
+
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
--- app/src/main/java/ua/khai/slesarev/bookfinder/HomeActivity.kt
+++ app/src/main/java/ua/khai/slesarev/bookfinder/HomeActivity.kt
@@ -1,7 +1,11 @@
 package ua.khai.slesarev.bookfinder
 
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.KeyEvent
+import androidx.core.content.ContextCompat
+import androidx.fragment.app.FragmentTransaction
 import androidx.navigation.findNavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.fragment.findNavController
@@ -9,6 +13,7 @@ import androidx.navigation.ui.setupWithNavController
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.google.android.material.search.SearchBar
 import com.google.android.material.search.SearchView
+import ua.khai.slesarev.bookfinder.dialogs.FullscreenDialog
 
 class HomeActivity : AppCompatActivity() {
 
@@ -16,7 +21,7 @@ class HomeActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
 
-        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottomNavigationView)
+        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottomNav)
 
         val homeScreenHostFrag = supportFragmentManager.findFragmentById(R.id.fragmentContainerView) as NavHostFragment
 
@@ -27,5 +32,28 @@ class HomeActivity : AppCompatActivity() {
         val searchBar = findViewById<SearchBar>(R.id.search_bar)
         val searchView = findViewById<SearchView>(R.id.search_view)
         searchView.setupWithSearchBar(searchBar)
+
+        searchBar.setOnMenuItemClickListener { item ->
+            when (item.itemId) {
+                R.id.action_search -> {
+
+                    val fragmentManager = supportFragmentManager
+                    val newFragment = FullscreenDialog()
+
+                    val transaction = fragmentManager.beginTransaction()
+                    transaction.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN)
+                    transaction
+                        .add(android.R.id.content, newFragment)
+                        .addToBackStack(null)
+                        .commit()
+
+                    true
+                }
+
+                else ->{
+                    false
+                }
+            }
+        }
     }
 }
